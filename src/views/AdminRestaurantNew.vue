225b67afd210dd35eba79ab1e118121ee9299b59
VinceLee9527 <vlee9527@gmail.com>
2021-11-14 00:48:24 +0800

    finished new restaurant submit page, added restaurant edit page

---
+++ src/views/AdminRestaurantNew.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container py-5">
+    <!-- 餐廳表單 AdminRestaurantForm -->
+    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from "./../components/AdminRestaurantForm.vue";
+
+export default {
+  components: {
+    AdminRestaurantForm,
+  },
+  methods: {
+    handleAfterSubmit(formData) {
+      //透過api把formData傳到server
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ": " + value);
+      }
+    },
+  },
+};
+</script>
