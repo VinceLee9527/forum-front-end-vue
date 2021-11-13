538619345ce49c2e9fa5dc4e95d7377769265544
VinceLee9527 <vlee9527@gmail.com>
2021-11-14 02:07:31 +0800

    finished edit user function

--- src/views/UserEdit.vue
+++ src/views/UserEdit.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="container py-5">
-    <form>
+    <form @submit.prevent.stop="handleSubmit">
       <div class="form-group">
         <label for="name">Name</label>
         <input
+          v-model="user.name"
           id="name"
           type="text"
           name="name"
@@ -15,12 +16,20 @@
 
       <div class="form-group">
         <label for="image">Image</label>
+        <img
+          v-if="user.image"
+          :src="user.image"
+          class="d-block img-thumbnail mb-3"
+          width="200"
+          height="200"
+        />
         <input
           id="image"
           type="file"
           name="image"
           accept="image/*"
           class="form-control-file"
+          @change="handleFileChange"
         />
       </div>
 
@@ -28,3 +37,63 @@
     </form>
   </div>
 </template>
+
+<script>
+const dummyUser = {
+  currentUser: {
+    id: 1,
+    name: "管理者",
+    email: "root@example.com",
+    image: "https://i.pravatar.cc/300",
+    isAdmin: true,
+  },
+  isAuthenticated: true,
+};
+
+export default {
+  data() {
+    return {
+      user: {
+        id: -1,
+        name: "",
+        image: "",
+      },
+    };
+  },
+  created() {
+    const { id } = this.$route.params;
+    this.fetchUser(id);
+  },
+  methods: {
+    handleSubmit(e) {
+      const form = e.target; // <form></form>
+      const formData = new FormData(form);
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ": " + value);
+      }
+    },
+    fetchUser(userId) {
+      console.log(userId);
+      const { currentUser } = dummyUser;
+      this.user = {
+        ...this.currentUser,
+        id: currentUser.id,
+        name: currentUser.name,
+        image: currentUser.image,
+      };
+    },
+    handleFileChange(e) {
+      const { files } = e.target;
+      if (files.length === 0) {
+        this.user.image = "";
+      } else {
+        const imgURL = window.URL.createObjectURL(files[0]);
+        this.user.image = imgURL;
+      }
+    },
+  },
+};
+</script>
+
+
+
